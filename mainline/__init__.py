8d6c93d9629251435e5826a9c1b4632c285d4584
Trevor Joynson (trevorj) <github@skywww.net>
2017-01-01 23:31:07 -0800

    Use PBR

--- mainline/__init__.py
+++ mainline/__init__.py
@@ -1,4 +1,5 @@
-from mainline._version import __version__
+import pkg_resources
+__version__ = pkg_resources.get_distribution(__name__).version
 
 from mainline.exceptions import DiError, UnresolvableError, UnprovidableError
 
